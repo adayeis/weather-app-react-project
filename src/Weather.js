8474f601833035b7d6b56fdab2113e3b623cc257
adayeis <136622255+adayeis@users.noreply.github.com>
2023-12-13 20:14:12 +0300

    basic css added

--- src/Weather.js
+++ src/Weather.js
@@ -4,13 +4,35 @@ export default function Weather() {
   return (
     <div className="Weather">
       <div>
-        <form>Search area</form>
+        <form>
+          <input tyoe="search" placeholder="Search a city..."></input>
+          <input type="submit" value="Search" />
+        </form>
       </div>
       <div>
         <h2>City name 📍</h2>
       </div>
-      <div>Current content</div>
-      <div>Forecast content</div>
+      <div>
+        <header>
+          <ul>
+            <li>21 September, Thursday</li>
+            <li>9:41 am</li>
+          </ul>
+        </header>
+        <div className="temp-container">
+          <ul>
+            <li className="current-temp">
+              20<span>°C</span>{" "}
+            </li>
+            <li>10° / 22°</li>
+            <li>Mostly sunny</li>
+          </ul>
+          <img
+            src="https://openweathermap.org/img/wn/10d@2x.png"
+            alt="description icon"
+          />
+        </div>
+      </div>
     </div>
   );
 }

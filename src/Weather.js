60547a14e709fe3605be63602f72521bbd445379
adayeis <136622255+adayeis@users.noreply.github.com>
2023-12-13 20:58:52 +0300

    axios and api

--- src/Weather.js
+++ src/Weather.js
@@ -1,38 +1,82 @@
+import { useState } from "react";
+import axios from "axios";
+import { Hearts } from "react-loader-spinner";
+
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <div>
-        <form>
-          <input tyoe="search" placeholder="Search a city..."></input>
-          <input type="submit" value="Search" />
-        </form>
+export default function Weather(props) {
+  const [weather, setWeather] = useState({ ready: false });
+  function displayTemperature(response) {
+    setWeather({
+      ready: true,
+      city: response.data.name,
+      temperature: Math.round(response.data.main.temp),
+      min: Math.round(response.data.main.temp_min),
+      max: Math.round(response.data.main.temp_max),
+      description: response.data.weather[0].description,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  if (weather.ready) {
+    return (
+      <div className="Weather">
+        <div>
+          <form>
+            <input tyoe="search" placeholder="Search a city..."></input>
+            <input type="submit" value="Search" />
+          </form>
+        </div>
+        <div>
+          <h2>{weather.city} 📍</h2>
+        </div>
+        <div>
+          <header>
+            <ul>
+              <li>21 September, Thursday</li>
+              <li>9:41 am</li>
+            </ul>
+          </header>
+          <div className="temp-container">
+            <ul>
+              <li className="current-temp">
+                {weather.temperature}
+                <span>°C</span>{" "}
+              </li>
+              <li>
+                {weather.min}° / {weather.max}°
+              </li>
+              <li className="description">{weather.description}</li>
+            </ul>
+            <img src={weather.icon} alt="description icon" />
+          </div>
+        </div>
       </div>
-      <div>
-        <h2>City name 📍</h2>
-      </div>
-      <div>
-        <header>
-          <ul>
-            <li>21 September, Thursday</li>
-            <li>9:41 am</li>
-          </ul>
-        </header>
-        <div className="temp-container">
-          <ul>
-            <li className="current-temp">
-              20<span>°C</span>{" "}
-            </li>
-            <li>10° / 22°</li>
-            <li>Mostly sunny</li>
-          </ul>
-          <img
-            src="https://openweathermap.org/img/wn/10d@2x.png"
-            alt="description icon"
+    );
+  } else {
+    const apiKey = "f3009e4852fa0a079dab291dabf020c4";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(displayTemperature);
+    return (
+      <div className="Weather">
+        <div>
+          <form>
+            <input tyoe="search" placeholder="Search a city..."></input>
+            <input type="submit" value="Search" />
+          </form>
+        </div>
+        <div className="loader">
+          <Hearts
+            height="100%"
+            width="100%"
+            radius="9"
+            color="#d3e9f5"
+            ariaLabel="three-dots-loading"
+            wrapperStyle
+            wrapperClass
           />
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }

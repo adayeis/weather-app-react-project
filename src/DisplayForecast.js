a238a33217b010f8874e77f64366bdbf31ba48a4
adayeis <136622255+adayeis@users.noreply.github.com>
2023-12-21 00:40:31 +0300

    forecast html css added

--- src/DisplayForecast.js
+++ src/DisplayForecast.js
@@ -16,6 +16,26 @@ export default function DisplayForecast() {
           <span className="forecast-max">19°C</span>{" "}
           <span className="forecast-min">12°C</span>
         </li>
+        <li>
+          <span>22 September 2023</span> <WeatherIcon code="01d" size={28} />
+          <span className="forecast-max">19°C</span>{" "}
+          <span className="forecast-min">12°C</span>
+        </li>
+        <li>
+          <span>22 September 2023</span> <WeatherIcon code="01d" size={28} />
+          <span className="forecast-max">19°C</span>{" "}
+          <span className="forecast-min">12°C</span>
+        </li>
+        <li>
+          <span>22 September 2023</span> <WeatherIcon code="01d" size={28} />
+          <span className="forecast-max">19°C</span>{" "}
+          <span className="forecast-min">12°C</span>
+        </li>
+        <li>
+          <span>22 September 2023</span> <WeatherIcon code="01d" size={28} />
+          <span className="forecast-max">19°C</span>{" "}
+          <span className="forecast-min">12°C</span>
+        </li>
       </ul>
     </div>
   );

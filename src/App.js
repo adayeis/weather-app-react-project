f4b1a8c2d2d049447111bd5f5146fe1c4d8690de
adayeis <136622255+adayeis@users.noreply.github.com>
2023-12-21 01:52:38 +0300

    footer added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ function App() {
   return (
     <div className="App">
       <Weather defaultCity="İstanbul" />
+      <footer>
+        coded by 👩🏻‍💻{" "}
+        <a href="https://adayeis.com/" target="_blank" rel="noreferrer">
+          {" "}
+          Ada Yeis
+        </a>{" "}
+        | open-sourced on{" "}
+        <a
+          href="https://github.com/adayeis/weather-app-react-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
